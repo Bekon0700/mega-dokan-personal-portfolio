3f489c2758644b27d6cc86c7086e4829e6b90ad1
Emdadul Islam <bekon1700@gmail.com>
2022-11-05 15:46:02 +0600

    products done

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
     const discount = (price * 1) * ((discountPercentage * 1) / 100)
     const discountPrice = (price * 1) - discount
   return (
-    <div className='w-5/6 lg:w-3/4 mx-auto py-14 lg:py-24'>
+    <div className='w-11/12 2xl:w-3/4 mx-auto py-14 lg:py-24'>
       <div className='grid grid-cols-12 gap-2'>
         <div className='col-span-12 lg:col-span-3 justify-center'>
           <img src={product.thumbnail} alt={`${product.productName}-thumbnail`} className='h-80 w-full shadow-2xl rounded-md'/>
@@ -26,11 +26,11 @@ const Products = () => {
           <p className='text-base font-semibold text-amber-800'>Stock: {product.stock}</p>
           <p className='text-sm font-normal text-amber-800'>Description: {product.description}</p>
         </div>
-        <div className='flex flex-col gap-4 col-span-12 lg:col-span-2 shadow-md p-4 rounded-2xl shadow-green-300 shadow-t-md'>
+        <div className='flex flex-col gap-4 justify-between col-span-12 lg:col-span-2 shadow-md p-4 rounded-2xl shadow-green-300 shadow-t-md'>
           <p className='text-2xl font-semibold text-gray-900'>${discountPrice.toFixed(2)}</p>
-          <p className='text-lg font-normal text-gray-900'>$5.50 Shipping & Import Fees Deposit to Bangladesh Details</p>
+          <p className='text-sm xl:text-base 2xl:text-lg font-normal text-gray-900'>$5.50 Shipping & Import Fees Deposit to Bangladesh Details</p>
           <p className='text-lg font-semibold text-gray-900'>Delivery within 7 working days</p>
-          <button onClick={() => cartHandler(product)} className='bg-yellow-200 text-xl font-semibold text-gray-900  py-2 rounded-md'>add to cart</button>
+          <button onClick={() => cartHandler(product)} className='bg-yellow-200 text-lg xl:text-xl font-semibold text-gray-900  py-2 rounded-md'>add to cart</button>
         </div>
       </div>
       <div className='border border-black my-12 rounded-md'>
