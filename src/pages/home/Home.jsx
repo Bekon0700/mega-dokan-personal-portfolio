48a1e7db8a20db9eda10f74311a39a5d374956e5
Emdadul Islam <bekon1700@gmail.com>
2022-11-06 14:29:34 +0600

    side nav fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,8 +8,7 @@ const Home = () => {
     <div className='w-11/12 lg:w-4/5 mx-auto pb-8'>
         <MegaDeal data={data} />
         <div className='flex items-center bg-orange-300 justify-center py-2 gap-4'>
-          <p className='text-base lg:text-xl font-semibold'>Find All our products </p>
-          <Link to={'/all-products'} className="btn glass text-gray-700 text-base lg:text-xl font-semibold">Here</Link>
+          <Link to={'/all-products'} className="text-gray-700 text-base lg:text-xl font-semibold">Find All our products Here</Link>
         </div>
     </div>
   )
