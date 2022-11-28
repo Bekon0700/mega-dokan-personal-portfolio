0dbc15b24e40d6a01dd53cc59bec84921a50246c
Emdadul Islam <bekon1700@gmail.com>
2022-11-29 00:15:43 +0600

    sidebar updated 4

--- src/components/slider/HomeSlider.jsx
+++ src/components/slider/HomeSlider.jsx
@@ -25,7 +25,7 @@ const HomeSlider = () => {
         cssEase: "ease-in"
     };
     return (
-        <div className='w-4/5 lg:w-3/4 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-0 items-center py-12'>
+        <div className='w-11/12 lg:w-3/4 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-0 items-center py-12'>
             <div className='w-5/6 '>
                 <p className='text-4xl lg:text-7xl font-light'>we bring <br/><span className='text-[#FFAE5D] font-bold'> your demand</span><br/> at your doorstep!</p>
             </div>
--- src/pages/search-products/SearchProducts.jsx
+++ src/pages/search-products/SearchProducts.jsx
@@ -1,26 +1,46 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/ProductCard'
 import { searchContext } from '../../context/SearchProvider'
 
 const SearchProducts = () => {
-    const param = useParams()
-    const [products, setProducts] = useState([])
-    const {searchKey} = useContext(searchContext)
-    useEffect(() => {
-        
-        const apiCall = async () => {
-            const res = await fetch(`https://inventory-api-personal.herokuapp.com/api/v1/products/search?item=${param.key}`)
-            const data = await res.json()
-            setProducts(data.products)
+    // const [products, setProducts] = useState([])
+    const { searchKey } = useContext(searchContext)
+    // useEffect(() => {
+
+    //     const apiCall = async () => {
+    //         const res = await fetch(`https://inventory-rest-api.vercel.app/api/v1/products/search?item=${param.key}`)
+    //         const data = await res.json()
+    //         setProducts(data.products)
+    //     }
+    //     apiCall()
+    // }, [searchKey])
+
+    const { data: products = [], isLoading, status } = useQuery({
+        queryKey: ['search', searchKey],
+        queryFn: async () => {
+            const res = await axios.get(`https://inventory-rest-api.vercel.app/api/v1/products/search?item=${searchKey}`)
+            return res.data.products
         }
-        apiCall()
-    }, [searchKey])
+    })
     return (
         <div className='w-5/6 mx-auto py-12'>
-
             <div className='flex flex-col gap-5'>
                 <p className='text-3xl font-bold text-center'>Our Products</p>
+                {
+                    isLoading &&
+                    <div className='h-screen flex justify-center items-center'>
+                        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4 "></div>
+                    </div>
+                }
+                {
+                    status == 'success' && products.length == 0 &&
+                    <div className='h-screen flex justify-center items-center'>
+                        <p className='text-3xl text-red-900 font-medium'>No Product found</p>
+                    </div>
+                }
                 <div className='py-12 grid grid-cols-1 lg:grid-cols-4 gap-8'>
                     {
                         products.map(el => <ProductCard key={el._id} product={el} megaDeal={false} />)
