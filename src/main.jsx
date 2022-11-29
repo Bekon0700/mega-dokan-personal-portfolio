6a29b45f56110b7c9aeafd75414330de4cdbea30
Emdadul Islam <bekon1700@gmail.com>
2022-11-29 21:20:21 +0600

    stripe payment added

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,25 @@ import SearchProvider from './context/SearchProvider'
 import CartProvider from './context/CartProvider'
 import AuthProvider from './context/AuthProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
 const queryClient = new QueryClient()
+
+const stripePromise = loadStripe(import.meta.env.VITE_stripe_pk);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <SearchProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </SearchProvider>
-      </CartProvider>
+      <Elements stripe={stripePromise}>
+        <CartProvider>
+          <SearchProvider>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </SearchProvider>
+        </CartProvider>
+      </Elements>
     </QueryClientProvider>
   </React.StrictMode>
 )
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,13 +1,97 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React, { useContext, useState } from 'react'
+import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
 import CartCard from '../../components/cart-card/CartCard'
+import { authContext } from '../../context/AuthProvider'
 import { cartContext } from '../../context/CartProvider'
 
 const Cart = () => {
+    const { user } = useContext(authContext)
     const { cart, totalPrice, cartClrHandler } = useContext(cartContext)
     const totalBill = totalPrice + 5
 
-    if(cart.length == 0){
+    const [paymentStatus, setPaymentStatus] = useState(false)
+    const [successStatus, setSuccessStatus] = useState('')
+    const [error, setError] = useState('')
+
+    const { data: paymentSecret } = useQuery({
+        queryKey: ['payment-secret', totalBill],
+        queryFn: async () => {
+            const res = await axios.post('https://inventory-rest-api.vercel.app/create-payment-intent', {
+                totalBill
+            })
+            return res.data
+        }
+    })
+    // console.log(paymentSecret)
+
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+            billing_details: {
+                name: user.displayName || '',
+                email: user.email || '',
+                phone: user.phoneNumber || '',
+            }
+        });
+
+        if (error) {
+            setError(error.message)
+        }
+
+        setPaymentStatus(true)
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            paymentSecret.client_secret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: user.displayName || '',
+                        email: user.email || '',
+                        phone: user.phoneNumber || '',
+                    }
+                },
+            },
+        );
+
+        if(paymentIntent?.status == 'succeeded'){
+            setSuccessStatus(paymentIntent.id)
+            toast.success('payment successful')
+            setError('')
+            console.log(successStatus)
+        }else{
+            setError(confirmError.message)
+            console.log(confirmError)
+            setSuccessStatus('')
+        }
+        setPaymentStatus(false)
+
+        card.clear()
+    }
+
+    if (cart.length == 0) {
         return (
             <div className='flex justify-center items-center h-screen'>
                 <p className='text-lg font-medium'>Please add some items to the cart first!</p>
@@ -64,10 +148,54 @@ const Cart = () => {
                             <p className='text-base font-medium'>Total Bill</p>
                             <p className='text-base font-bold'>${totalBill.toFixed(2)}</p>
                         </div>
-                        <Link to='/checkout' className='bg-blue-700 py-2 text-sm lg:text-base uppercase font-medium text-white w-full rounded-sm text-center'>Click here to proceed </Link>
-
+                        {/* The button to open modal */}
+                        {
+                            user ?
+                                <label htmlFor="my-modal" className='bg-blue-700 py-2 text-sm lg:text-base uppercase font-medium text-white w-full rounded-sm text-center cursor-pointer'>Click here to proceed</label>
+                                :
+                                <Link to='/login' state={{ from: `${location.pathname}` }} className='bg-gray-800 py-2 text-sm lg:text-sm uppercase font-medium text-white w-full rounded-sm text-center cursor-pointer'>Please Sign in to process payment</Link>
+                        }
                     </div>
-
+                </div>
+            </div>
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box">
+                    <div className='flex justify-between items-center'>
+                        <p className='text-base font-bold'>Pay ${totalBill.toFixed(2)}</p>
+                        <label htmlFor="my-modal" onClick={cartClrHandler} className="bg-red-700 text-white px-2 py-1 rounded-md text-sm font-bold">X</label>
+                    </div>
+                    <p className='py-4 text-sm text-gray-500'>Please fill in your information carefully</p>
+                    <form onSubmit={handleSubmit} className='flex flex-col gap-4 border py-4 px-2'>
+                        <input type="text" value={user.displayName || 'name'} disabled className='text-base text-gray-400 cursor-no-drop' />
+                        <input type="text" value={user.email || 'email'} disabled className='text-base text-gray-400 cursor-no-drop' />
+                        <CardElement
+                            options={{
+                                style: {
+                                    base: {
+                                        fontSize: '16px',
+                                        color: '#424770',
+                                        '::placeholder': {
+                                            color: '#aab7c4',
+                                        },
+                                    },
+                                    invalid: {
+                                        color: '#9e2146',
+                                    },
+                                },
+                            }}
+                        />
+                        <button type="submit" disabled={!stripe} className={paymentStatus ? 'px-4 py-1 bg-gray-800 text-white cursor-wait' : 'px-4 py-1 bg-gray-800 text-white'}>
+                            Pay
+                        </button>
+                        {
+                            error && <p className='text-center text-sm font-medium text-red-500'>{error}</p>
+                        }
+                        {
+                            successStatus && <p className='text-left text-sm font-normal text-green-700'>Payment Success <br/>Transection Id: <span >{successStatus}</span></p>
+                        }
+                    </form>
                 </div>
             </div>
         </div>
